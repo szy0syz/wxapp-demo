fa06b5e77967dd792cfd38e810824296e91b3ce5
szy0syz <szy0syz@gmail.com>
2017-07-14 23:46:36 +0800

    added v2ex-project

---
+++ v2ex/pages/index/index.js
@@ -0,0 +1,294 @@
+//index.js
+//获取应用实例
+var app = getApp()
+
+Page({
+  data: {
+    
+    hot:
+    [
+      {
+        "id": 375213,
+        "title": "小区车位只有 70 年使用权，会影响后期交易吗",
+        "url": "http://www.v2ex.com/t/375213",
+        "content": "RT，14 年的时候小区车位大概 11w 左右，现在 13w 了，想买一个，但是只有 70 年使用权没有产权，有点犹豫，怕后期不好交易。想听听大家的建议",
+        "content_rendered": "\u003Cp\u003ERT，14 年的时候小区车位大概 11w 左右，现在 13w 了，想买一个，但是只有 70 年使用权没有产权，有点犹豫，怕后期不好交易。想听听大家的建议\u003C/p\u003E\u000A",
+        "replies": 147,
+        "member": {
+          "id": 172935,
+          "username": "yangxiaoluck",
+          "tagline": "",
+          "avatar_mini": "//v2ex.assets.uxengine.net/avatar/0eed/dedc/172935_mini.png?m=1463801687",
+          "avatar_normal": "//v2ex.assets.uxengine.net/avatar/0eed/dedc/172935_normal.png?m=1463801687",
+          "avatar_large": "//v2ex.assets.uxengine.net/avatar/0eed/dedc/172935_large.png?m=1463801687"
+        },
+        "node": {
+          "id": 557,
+          "name": "life",
+          "title": "生活",
+          "title_alternative": "Life",
+          "url": "http://www.v2ex.com/go/life",
+          "topics": 507,
+          "avatar_mini": "//v2ex.assets.uxengine.net/navatar/6e27/13a6/557_mini.png?m=1473710080",
+          "avatar_normal": "//v2ex.assets.uxengine.net/navatar/6e27/13a6/557_normal.png?m=1473710080",
+          "avatar_large": "//v2ex.assets.uxengine.net/navatar/6e27/13a6/557_large.png?m=1473710080"
+        },
+        "created": 1499991466,
+        "last_modified": 1499991466,
+        "last_touched": 1500040173
+      },
+
+      {
+        "id": 375275,
+        "title": "大家都来说说你最喜欢的名人名言",
+        "url": "http://www.v2ex.com/t/375275",
+        "content": "我先来一个吧\u000D\u000A\u000D\u000A# \u0022我不同意你的观点，但是我誓死捍卫你说话的权利\u0022 \u002D 伏尔泰\u000D\u000A\u000D\u000AUi5JLlAgDQrmhL/lpKnloILmsqHmnInkvaDnmoTmlYzkuro\u003D",
+        "content_rendered": "\u003Cp\u003E我先来一个吧\u003C/p\u003E\u000A\u003Ch1\u003E\u0022我不同意你的观点，但是我誓死捍卫你说话的权利\u0022 \u002D 伏尔泰\u003C/h1\u003E\u000A\u003Cp\u003EUi5JLlAgDQrmhL/lpKnloILmsqHmnInkvaDnmoTmlYzkuro\u003D\u003C/p\u003E\u000A",
+        "replies": 144,
+        "member": {
+          "id": 19281,
+          "username": "yylzcom",
+          "tagline": "",
+          "avatar_mini": "//v2ex.assets.uxengine.net/avatar/be6e/a238/19281_mini.png?m=1389431882",
+          "avatar_normal": "//v2ex.assets.uxengine.net/avatar/be6e/a238/19281_normal.png?m=1389431882",
+          "avatar_large": "//v2ex.assets.uxengine.net/avatar/be6e/a238/19281_large.png?m=1389431882"
+        },
+        "node": {
+          "id": 12,
+          "name": "qna",
+          "title": "问与答",
+          "title_alternative": "Questions and Answers",
+          "url": "http://www.v2ex.com/go/qna",
+          "topics": 89905,
+          "avatar_mini": "//v2ex.assets.uxengine.net/navatar/c20a/d4d7/12_mini.png?m=1499949798",
+          "avatar_normal": "//v2ex.assets.uxengine.net/navatar/c20a/d4d7/12_normal.png?m=1499949798",
+          "avatar_large": "//v2ex.assets.uxengine.net/navatar/c20a/d4d7/12_large.png?m=1499949798"
+        },
+        "created": 1500000882,
+        "last_modified": 1500000966,
+        "last_touched": 1500041136
+      },
+
+      {
+        "id": 375325,
+        "title": "Java 因为含“av\u0022被微博屏蔽了",
+        "url": "http://www.v2ex.com/t/375325",
+        "content": "刚试了好多个词，只要含\u0022av\u0022都被屏蔽了，除了 javascript 😂\u000D\u000A微博要开始搞文字狱了吗",
+        "content_rendered": "刚试了好多个词，只要含\u0026quot\u003Bav\u0026quot\u003B都被屏蔽了，除了 javascript 😂\u000D\u003Cbr /\u003E微博要开始搞文字狱了吗",
+        "replies": 105,
+        "member": {
+          "id": 165141,
+          "username": "AscenZ",
+          "tagline": "life\u0027s a struggle",
+          "avatar_mini": "//v2ex.assets.uxengine.net/avatar/4cde/1a0e/165141_mini.png?m=1467633985",
+          "avatar_normal": "//v2ex.assets.uxengine.net/avatar/4cde/1a0e/165141_normal.png?m=1467633985",
+          "avatar_large": "//v2ex.assets.uxengine.net/avatar/4cde/1a0e/165141_large.png?m=1467633985"
+        },
+        "node": {
+          "id": 300,
+          "name": "programmer",
+          "title": "程序员",
+          "title_alternative": "Programmer",
+          "url": "http://www.v2ex.com/go/programmer",
+          "topics": 16390,
+          "avatar_mini": "//v2ex.assets.uxengine.net/navatar/94f6/d7e0/300_mini.png?m=1499949650",
+          "avatar_normal": "//v2ex.assets.uxengine.net/navatar/94f6/d7e0/300_normal.png?m=1499949650",
+          "avatar_large": "//v2ex.assets.uxengine.net/navatar/94f6/d7e0/300_large.png?m=1499949650"
+        },
+        "created": 1500009883,
+        "last_modified": 1500021957,
+        "last_touched": 1500041147
+      },
+
+      {
+        "id": 375224,
+        "title": "程序员创业启动资金怎么解决？",
+        "url": "http://www.v2ex.com/t/375224",
+        "content": "本人码农，想想找个 UI 都都找不到。。\u000D\u000A\u000D\u000A\u000D\u000A喷子绕道，谢谢",
+        "content_rendered": "本人码农，想想找个 UI 都都找不到。。\u000D\u003Cbr /\u003E\u000D\u003Cbr /\u003E\u000D\u003Cbr /\u003E喷子绕道，谢谢",
+        "replies": 71,
+        "member": {
+          "id": 156857,
+          "username": "mortonnex",
+          "tagline": "",
+          "avatar_mini": "//v2ex.assets.uxengine.net/avatar/67b8/6995/156857_mini.png?m=1461910632",
+          "avatar_normal": "//v2ex.assets.uxengine.net/avatar/67b8/6995/156857_normal.png?m=1461910632",
+          "avatar_large": "//v2ex.assets.uxengine.net/avatar/67b8/6995/156857_large.png?m=1461910632"
+        },
+        "node": {
+          "id": 300,
+          "name": "programmer",
+          "title": "程序员",
+          "title_alternative": "Programmer",
+          "url": "http://www.v2ex.com/go/programmer",
+          "topics": 16390,
+          "avatar_mini": "//v2ex.assets.uxengine.net/navatar/94f6/d7e0/300_mini.png?m=1499949650",
+          "avatar_normal": "//v2ex.assets.uxengine.net/navatar/94f6/d7e0/300_normal.png?m=1499949650",
+          "avatar_large": "//v2ex.assets.uxengine.net/navatar/94f6/d7e0/300_large.png?m=1499949650"
+        },
+        "created": 1499993917,
+        "last_modified": 1499993917,
+        "last_touched": 1500031023
+      },
+
+      {
+        "id": 375256,
+        "title": "阿里云你不管干什么都要收费，这样好吗？",
+        "url": "http://www.v2ex.com/t/375256",
+        "content": "这阿里云，快照要收费，查个漏洞收费，防火墙收费，安全管家收费，什么都要收费，而且都是分开收的，还动不动就几千几万的。这让我们个人开发者情何以堪，这样真的好吗？ O__O \u0022…",
+        "content_rendered": "这阿里云，快照要收费，查个漏洞收费，防火墙收费，安全管家收费，什么都要收费，而且都是分开收的，还动不动就几千几万的。这让我们个人开发者情何以堪，这样真的好吗？ O__O \u0026quot\u003B…",
+        "replies": 68,
+        "member": {
+          "id": 156814,
+          "username": "Hanggi",
+          "tagline": "",
+          "avatar_mini": "//v2ex.assets.uxengine.net/avatar/85f6/cdb0/156814_mini.png?m=1499746915",
+          "avatar_normal": "//v2ex.assets.uxengine.net/avatar/85f6/cdb0/156814_normal.png?m=1499746915",
+          "avatar_large": "//v2ex.assets.uxengine.net/avatar/85f6/cdb0/156814_large.png?m=1499746915"
+        },
+        "node": {
+          "id": 104,
+          "name": "cloud",
+          "title": "云计算",
+          "title_alternative": "Cloud Computing",
+          "url": "http://www.v2ex.com/go/cloud",
+          "topics": 2310,
+          "avatar_mini": "//v2ex.assets.uxengine.net/navatar/c9e1/074f/104_mini.png?m=1499949819",
+          "avatar_normal": "//v2ex.assets.uxengine.net/navatar/c9e1/074f/104_normal.png?m=1499949819",
+          "avatar_large": "//v2ex.assets.uxengine.net/navatar/c9e1/074f/104_large.png?m=1499949819"
+        },
+        "created": 1499999124,
+        "last_modified": 1499999153,
+        "last_touched": 1500040122
+      },
+
+      {
+        "id": 375329,
+        "title": "这两天各大互联网公司是不是都在急招 AC 自动机工程师？",
+        "url": "http://www.v2ex.com/t/375329",
+        "content": "不禁想念两句诗",
+        "content_rendered": "\u003Cp\u003E不禁想念两句诗\u003C/p\u003E\u000A",
+        "replies": 56,
+        "member": {
+          "id": 169110,
+          "username": "Morriaty",
+          "tagline": "",
+          "avatar_mini": "//v2ex.assets.uxengine.net/avatar/64e7/d277/169110_mini.png?m=1495696257",
+          "avatar_normal": "//v2ex.assets.uxengine.net/avatar/64e7/d277/169110_normal.png?m=1495696257",
+          "avatar_large": "//v2ex.assets.uxengine.net/avatar/64e7/d277/169110_large.png?m=1495696257"
+        },
+        "node": {
+          "id": 300,
+          "name": "programmer",
+          "title": "程序员",
+          "title_alternative": "Programmer",
+          "url": "http://www.v2ex.com/go/programmer",
+          "topics": 16390,
+          "avatar_mini": "//v2ex.assets.uxengine.net/navatar/94f6/d7e0/300_mini.png?m=1499949650",
+          "avatar_normal": "//v2ex.assets.uxengine.net/navatar/94f6/d7e0/300_normal.png?m=1499949650",
+          "avatar_large": "//v2ex.assets.uxengine.net/navatar/94f6/d7e0/300_large.png?m=1499949650"
+        },
+        "created": 1500010794,
+        "last_modified": 1500010867,
+        "last_touched": 1500033019
+      },
+
+      {
+        "id": 375343,
+        "title": "帮我把相片换个底色吧！",
+        "url": "http://www.v2ex.com/t/375343",
+        "content": "刚才一个高中同学发给我一个证件照，说是要换成白色，还说什么你们学计算机的不是很容易么。\u000D\u000A当时我一看，这™不是画图的么，我让他找个会 ps 的去弄吧，然后人家就不乐意了，一顿埋汰，当时我就生气了，欺负我们程序猿不会 PS 这是，啪啪啪写了 10 行 Python 直接给换成白底了，还告诉他，这尼玛精确到像素级别的。\u000D\u000A\u000D\u000A不过之前还好，都是找修电脑，现在直接 P 图了，V 友们这些同学怎么破，大家怎么看好久不联系，一找你就修电脑屁图的？",
+        "content_rendered": "刚才一个高中同学发给我一个证件照，说是要换成白色，还说什么你们学计算机的不是很容易么。\u000D\u003Cbr /\u003E当时我一看，这™不是画图的么，我让他找个会 ps 的去弄吧，然后人家就不乐意了，一顿埋汰，当时我就生气了，欺负我们程序猿不会 PS 这是，啪啪啪写了 10 行 Python 直接给换成白底了，还告诉他，这尼玛精确到像素级别的。\u000D\u003Cbr /\u003E\u000D\u003Cbr /\u003E不过之前还好，都是找修电脑，现在直接 P 图了，V 友们这些同学怎么破，大家怎么看好久不联系，一找你就修电脑屁图的？",
+        "replies": 43,
+        "member": {
+          "id": 174386,
+          "username": "liuhao2050qq",
+          "tagline": "",
+          "avatar_mini": "//v2ex.assets.uxengine.net/gravatar/fa51958fc7dece170379bcb422e6d3e2?s=24&d=retro",
+          "avatar_normal": "//v2ex.assets.uxengine.net/gravatar/fa51958fc7dece170379bcb422e6d3e2?s=48&d=retro",
+          "avatar_large": "//v2ex.assets.uxengine.net/gravatar/fa51958fc7dece170379bcb422e6d3e2?s=73&d=retro"
+        },
+        "node": {
+          "id": 16,
+          "name": "share",
+          "title": "分享发现",
+          "title_alternative": "Share",
+          "url": "http://www.v2ex.com/go/share",
+          "topics": 22451,
+          "avatar_mini": "//v2ex.assets.uxengine.net/navatar/c74d/97b0/16_mini.png?m=1499949759",
+          "avatar_normal": "//v2ex.assets.uxengine.net/navatar/c74d/97b0/16_normal.png?m=1499949759",
+          "avatar_large": "//v2ex.assets.uxengine.net/navatar/c74d/97b0/16_large.png?m=1499949759"
+        },
+        "created": 1500014391,
+        "last_modified": 1500014391,
+        "last_touched": 1500036892
+      },
+
+      {
+        "id": 375202,
+        "title": "20170714 午夜俱乐部",
+        "url": "http://www.v2ex.com/t/375202",
+        "content": "",
+        "content_rendered": "",
+        "replies": 38,
+        "member": {
+          "id": 131562,
+          "username": "dbdd",
+          "tagline": "",
+          "avatar_mini": "//v2ex.assets.uxengine.net/avatar/ece7/1105/131562_mini.png?m=1449393673",
+          "avatar_normal": "//v2ex.assets.uxengine.net/avatar/ece7/1105/131562_normal.png?m=1449393673",
+          "avatar_large": "//v2ex.assets.uxengine.net/avatar/ece7/1105/131562_large.png?m=1449393673"
+        },
+        "node": {
+          "id": 286,
+          "name": "afterdark",
+          "title": "天黑以后",
+          "title_alternative": "After Dark",
+          "url": "http://www.v2ex.com/go/afterdark",
+          "topics": 2262,
+          "avatar_mini": "//v2ex.assets.uxengine.net/navatar/16a5/cdae/286_mini.png?m=1433876741",
+          "avatar_normal": "//v2ex.assets.uxengine.net/navatar/16a5/cdae/286_normal.png?m=1433876741",
+          "avatar_large": "//v2ex.assets.uxengine.net/navatar/16a5/cdae/286_large.png?m=1433876741"
+        },
+        "created": 1499961987,
+        "last_modified": 1499961987,
+        "last_touched": 1499999585
+      },
+
+      {
+        "id": 375229,
+        "title": "[TEST] 刘晓波",
+        "url": "http://www.v2ex.com/t/375229",
+        "content": "just for test.",
+        "content_rendered": "\u003Cp\u003Ejust for test.\u003C/p\u003E\u000A",
+        "replies": 35,
+        "member": {
+          "id": 175314,
+          "username": "xss",
+          "tagline": "",
+          "avatar_mini": "//v2ex.assets.uxengine.net/gravatar/9ca7633b54737bc5c68b79f9f58c8226?s=24&d=retro",
+          "avatar_normal": "//v2ex.assets.uxengine.net/gravatar/9ca7633b54737bc5c68b79f9f58c8226?s=48&d=retro",
+          "avatar_large": "//v2ex.assets.uxengine.net/gravatar/9ca7633b54737bc5c68b79f9f58c8226?s=73&d=retro"
+        },
+        "node": {
+          "id": 116,
+          "name": "wikipedia",
+          "title": "Wikipedia",
+          "title_alternative": "Wikipedia",
+          "url": "http://www.v2ex.com/go/wikipedia",
+          "topics": 60,
+          "avatar_mini": "//v2ex.assets.uxengine.net/navatar/c451/47de/116_mini.png?m=1404136721",
+          "avatar_normal": "//v2ex.assets.uxengine.net/navatar/c451/47de/116_normal.png?m=1404136721",
+          "avatar_large": "//v2ex.assets.uxengine.net/navatar/c451/47de/116_large.png?m=1404136721"
+        },
+        "created": 1499994575,
+        "last_modified": 1499994575,
+        "last_touched": 1499997407
+      }
+    ]
+  },
+  onLoad: function () {
+    var _this = this;
+
+  },
+})
